ab20e83a12bfa863022ec96bc5f793c360b79659
Phước Lê <phuoclv264@gmail.com>
2025-09-12 10:46:32 +0000

    Tôi bị rất nhiều lỗi liên quan đến apple touch icon, bạn kiểm tra lại gi

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/apple-touch-icon.png',
+        destination: 'https://firebasestorage.googleapis.com/v0/b/katrinaone.firebasestorage.app/o/logo_coffee.png?alt=media&token=d32a3b76-55ff-41f4-984f-a2c2742b6532',
+        permanent: true,
+      },
+    ]
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
