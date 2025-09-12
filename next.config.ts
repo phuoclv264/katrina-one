219d74ffaad51d5ff95282d6a3f23f657f9e7eec
Phước Lê <phuoclv264@gmail.com>
2025-09-12 10:52:22 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source: '/apple-touch-icon:path(*).png',
+        source: '/apple-touch-icon:path*.png',
         destination: 'https://firebasestorage.googleapis.com/v0/b/katrinaone.firebasestorage.app/o/logo_coffee.png?alt=media&token=d32a3b76-55ff-41f4-984f-a2c2742b6532',
         permanent: true,
       },
