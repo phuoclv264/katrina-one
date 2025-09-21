7f97e7f4695cb6432a4a514e32fcf0379da1faea
Phước Lê <phuoclv264@gmail.com>
2025-09-21 16:06:15 +0000

    use-fcm.ts có hoạt động trên trình duyệt máy tính không? có thư viện rea

--- src/hooks/use-fcm.ts
+++ src/hooks/use-fcm.ts
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { useEffect, useState } from 'use-sync-external-store/extra';
@@ -31,7 +30,7 @@ export const useFcm = () => {
                     <div
                       className={cn(
                         'max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5',
-                        t.visible ? 'animate-enter' : 'animate-leave'
+                        t.visible ? 'animate-in fade-in' : 'animate-out fade-out'
                       )}
                     >
                         <div className="flex-1 w-0 p-4">
@@ -51,7 +50,7 @@ export const useFcm = () => {
                             onClick={() => toast.dismiss(t.id)}
                             className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
                         >
-                            Close
+                            Đóng
                         </button>
                         </div>
                     </div>
