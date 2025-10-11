c3b09b9fad86651ad065a848e82e162846293993
Phước Lê <phuoclv264@gmail.com>
2025-10-11 07:48:54 +0000

    trong shift-info-dialog, Các nút "Đổi ca" và "Nhờ nhận ca" đã được cập n

--- src/app/(app)/schedule/_components/shift-info-dialog.tsx
+++ src/app/(app)/schedule/_components/shift-info-dialog.tsx
@@ -163,7 +163,7 @@ export default function ShiftInfoDialog({
                                     </div>
                                     {canSwap && (
                                         <Button size="sm" onClick={() => handlePassRequest(user, true)} disabled={isProcessing || isThisUserProcessing || alreadyRequested}>
-                                            {isThisUserProcessing ? <Loader2 className="h-4 w-4 animate-spin"/> : <Replace className="mr-2 h-4 w-4" />}
+                                            {isThisUserProcessing || isProcessing ? <Loader2 className="h-4 w-4 animate-spin mr-2"/> : <Replace className="mr-2 h-4 w-4" />}
                                             {alreadyRequested ? 'Đã nhờ' : 'Đổi ca'}
                                         </Button>
                                     )}
@@ -192,7 +192,7 @@ export default function ShiftInfoDialog({
                                             <p className="text-sm text-muted-foreground">{user.role}</p>
                                         </div>
                                         <Button size="sm" onClick={() => handlePassRequest(user, false)} disabled={isProcessing || isThisUserProcessing || alreadyRequested}>
-                                            {isThisUserProcessing ? <Loader2 className="h-4 w-4 animate-spin"/> : <Send className="mr-2 h-4 w-4" />}
+                                            {isThisUserProcessing || isProcessing ? <Loader2 className="h-4 w-4 animate-spin mr-2"/> : <Send className="mr-2 h-4 w-4" />}
                                             {alreadyRequested ? 'Đã nhờ' : 'Nhờ nhận ca'}
                                         </Button>
                                     </CardContent>
