97183e8c384ce23f84f192756c8a79618c28443e
phuoclv264 <phuoclv264@gmail.com>
2025-11-27 21:41:21 +0700

    Fix keyboard error on mobile app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,17 @@
 import type { CapacitorConfig, PluginsConfig } from '@capacitor/cli';
+import { KeyboardResize, KeyboardStyle } from '@capacitor/keyboard';
 
 const config: CapacitorConfig = {
   appId: 'com.katrinaone.app',
   appName: 'Katrina One',
   webDir: 'out',
+  plugins: {
+    Keyboard: {
+      resize: KeyboardResize.Body,
+      style: KeyboardStyle.Dark,
+      resizeOnFullScreen: true,
+    }
+  },
   android: {
     adjustMarginsForEdgeToEdge: "auto",
   }
